a6363c8a86d479c777bb83634ccf942e33f61356
Ben Baker <bakerb845@gmail.com>
2019-12-30 10:10:34 -0700

    dealing with std::make_unique on old gcc systems

---
+++ include/mflib/private/private.hpp
@@ -0,0 +1,14 @@
+#ifndef MFLIB_PRIVATE_PRIVATE_HPP
+#define MFLIB_PRIVATE_PRIVATE_HPP
+#ifndef HAVE_STD__MAKE_UNIQUE
+#include <memory>
+namespace std
+{
+template<typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args&&... args)
+{
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+}
+#endif
+#endif
