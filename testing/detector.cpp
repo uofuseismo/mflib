958978e0ca1d4c58f47d4ae5a60f55259a44840b
Ben Baker <bakerb845@gmail.com>
2020-01-28 08:56:08 -0700

    adding a unit test and kicking up the patch level.

--- testing/detector.cpp
+++ testing/detector.cpp
@@ -14,19 +14,23 @@ TEST(singleChannelDetection, detectorParameters)
     DetectorParameters parms;
     int minSamples = 50;
     double thresh = 0.8;
+    bool getWaveform = true;
     auto policy = MFLib::MaximumMatchedFilterPolicy::ABSOLUTE_MAXIMUM; 
     EXPECT_NO_THROW(parms.setMinimumDetectionSpacing(minSamples));
     EXPECT_NO_THROW(parms.setDetectionThreshold(thresh));
     EXPECT_NO_THROW(parms.setMaximaPolicy(policy));
+    parms.toggleGetDetectedWaveform(getWaveform);
 
     EXPECT_EQ(parms.getMinimumDetectionSpacing(), minSamples);
     EXPECT_NEAR(parms.getDetectionThreshold(), thresh, 1.e-14);
     EXPECT_EQ(parms.getMaximaPolicy(), policy);
+    EXPECT_TRUE(parms.getDetectedWaveform());
 
     DetectorParameters parmsCopy(parms);
     EXPECT_EQ(parmsCopy.getMinimumDetectionSpacing(), minSamples);
     EXPECT_NEAR(parmsCopy.getDetectionThreshold(), thresh, 1.e-14);
     EXPECT_EQ(parmsCopy.getMaximaPolicy(), policy);
+    EXPECT_TRUE(parmsCopy.getDetectedWaveform());
 
 }
 
